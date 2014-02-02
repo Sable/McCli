533445d814ddda9e025acab5b9b2b269afa7a00f
Tristan Labelle <tristan@tristanlabelle.com>
2014-02-02 12:16:25 -0500

    Added missing IndexCall file.

---
+++ McCli.Compilation/IR/IndexCall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McCli.Compilation.IR
+{
+	/// <summary>
+	/// An expression in which either a variable is indexed.
+	/// Represents both array accesses and function handle invocations (syntactic ambiguity).
+	/// </summary>
+	public sealed class IndexCall : Assignment
+	{
+		public readonly Variable Subject;
+		public readonly ImmutableArray<Variable> Arguments;
+		public readonly ImmutableArray<Variable> Targets;
+
+		public IndexCall(Variable subject, ImmutableArray<Variable> arguments, ImmutableArray<Variable> targets)
+		{
+			this.Subject = subject;
+			this.Arguments = arguments;
+			this.Targets = targets;
+		}
+
+		public override void Accept(Visitor visitor)
+		{
+			visitor.VisitIndexCall(this);
+		}
+	}
+}
