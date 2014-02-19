4dcf69a18d7b9cfc6178458fe0764ed3a292103b
Tristan Labelle <tristan@tristanlabelle.com>
2014-02-19 10:11:32 -0500

    Started implementing Tamer IR XML reading. Added a benchmarks folder with fahrenheit2degree.

---
+++ Benchmarks/fahrenheit2degree.m
@@ -0,0 +1,3 @@
+function degree = fahrenheit2degree(fahrenheit)
+	degree = (fahrenheit + 40) * 5 / 9 - 40;
+end
---
+++ McCli.Compilation/IR/TamerXmlReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace McCli.Compilation.IR
+{
+	/// <summary>
+	/// Reads Tamer IR from its XML-serialized representation.
+	/// </summary>
+	public sealed class TamerXmlReader
+	{
+		public static IEnumerable<Function> Read(Stream stream)
+		{
+			Contract.Requires(stream != null && stream.CanRead);
+
+			var document = XDocument.Load(stream);
+			var root = document.Root;
+			Contract.Assert(root != null && root.Name == "CompilationUnit");
+
+			foreach (var functionElement in root.Elements("TIRFunction"))
+				yield return ReadFunction(functionElement);
+		}
+
+		private static Function ReadFunction(XElement functionElement)
+		{
+			Contract.Requires(functionElement != null && functionElement.Name == "TIRFunction");
+
+			var variables = new Dictionary<string, Variable>();
+
+			var variablesElement = functionElement.Element("Variables");
+			if (variablesElement != null)
+			{
+				foreach (var variableElement in variablesElement.Elements("Variable"))
+				{
+					var variable = ReadVariable(variableElement);
+					variables.Add(variable.Name, variable);
+				}
+			}
+
+			throw new NotImplementedException();
+		}
+
+		private static Variable ReadVariable(XElement variableElement)
+		{
+			var name = (string)variableElement.Attribute("name");
+
+			MType type = null;
+			var className = (string)variableElement.Attribute("class");
+			var complex = (bool?)variableElement.Attribute("complex");
+			if (className != null && complex.HasValue)
+			{
+				type = MClass.FromName((string)className);
+				if (complex.Value) type = ((MPrimitiveClass)type).Complex;
+			}
+
+			// Scalar, constant value
+			throw new NotImplementedException();
+		}
+	}
+}
