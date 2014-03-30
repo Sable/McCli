f7e5c8ec4901a6ab331c763950ea92a4d34f164e
Tristan Labelle <tristan@tristanlabelle.com>
2014-03-30 19:52:17 -0400

    Added a BenchmarkRunner project.

---
+++ McCli.BenchmarkRunner/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace McCli.BenchmarkRunner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 2)
+			{
+				Console.WriteLine("Usage: {0} path scale", Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location));
+				return;
+			}
+
+			string benchmarkPath = args[0];
+			var benchmarkAssembly = Assembly.LoadFile(benchmarkPath);
+			var type = benchmarkAssembly.GetExportedTypes().Single();
+			var method = type.GetMethod(type.Name);
+			var @delegate = (Action<double>)Delegate.CreateDelegate(typeof(Action<double>), method);
+
+			@delegate(0);
+
+			double scale = int.Parse(args[1]);
+
+			var stopwatch = Stopwatch.StartNew();
+			try
+			{
+				@delegate(scale);
+			}
+			catch (Exception exception)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(exception);
+				Console.ResetColor();
+				return;
+			}
+
+			var duration = stopwatch.Elapsed;
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Completed in {0}", duration);
+			Console.ResetColor();
+		}
+	}
+}
